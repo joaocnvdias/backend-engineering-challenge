83d337657b9a8af29c424fd0b14b3202cfb2940a
Dias <joaocnvdias@gmail.com>
2026-06-04 18:44:52 +0100

    refactor: create dedicated module for parsing

--- unbabel_cli/__main__.py
+++ unbabel_cli/__main__.py
@@ -1,21 +1,9 @@
-import argparse
-import json
+from parser import parse_from_cli, load_translation_events
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input_file", type=str, help="Input file location with translation events")
-    parser.add_argument("--window_size", type=int, help="Temporal window size for computing average delivery time")
-    args = parser.parse_args()
-
-    input_file_name= args.input_file
-    window_size = args.window_size
-    translation_events = []
-
-    with open(input_file_name, "r", encoding="utf-8") as f:
-        for line in f:
-            event = json.loads(line)
-            translation_events.append(event) #keeps order
-
+    args = parse_from_cli()
+    translation_events = load_translation_events(file_location=args.input_file)
+    print(translation_events)
 if __name__ == "__main__":
     main()
 
