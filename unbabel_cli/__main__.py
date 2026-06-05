a34962b7581973f3f66b944232e80ab851c9e1c8
Dias <joaocnvdias@gmail.com>
2026-06-05 18:05:22 +0100

    feat: added JSONL output file logic

--- unbabel_cli/__main__.py
+++ unbabel_cli/__main__.py
@@ -1,5 +1,7 @@
 from parser import parse_from_cli, load_translation_events, filter_events_list
 from window import sliding_window_loop
+from utils import unix_to_timestamp
+import json
 
 def main():
 
@@ -8,6 +10,15 @@ def main():
     filtered_events, starting_minute, final_minute = filter_events_list(events_list=translation_events)
     averages_list = sliding_window_loop(starting_minute=starting_minute, final_minute=final_minute, window_size= args.window_size, filtered_events=filtered_events)
     
+    for item in averages_list:
+        item["date"] = unix_to_timestamp(unix = item["date"], output= True)
+        
+    with open('data/output.jsonl', 'w') as outfile:
+        for entry in averages_list:
+            json.dump(entry, outfile)
+            if entry != averages_list[-1]:
+                outfile.write('\n') #keep requested output structure
+
 if __name__ == "__main__":
     main()
 
--- unbabel_cli/utils.py
+++ unbabel_cli/utils.py
@@ -4,8 +4,10 @@ def timestamp_to_unix(timestamp: str) -> float:
     dt = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f") #%f represents microseconds
     return dt.replace(tzinfo=datetime.timezone.utc).timestamp() #assuming utc timezone to assure consistency in results between different machines
 
-def unix_to_timestamp(unix: float) -> str:
+def unix_to_timestamp(unix: float, output = False) -> str:
     dt = datetime.datetime.fromtimestamp(unix, tz=datetime.timezone.utc)
+    if output: 
+        return dt.strftime("%Y-%m-%d %H:%M:%S")
     return dt.strftime("%Y-%m-%d %H:%M:%S.%f")
 
 def floor_to_minutes(unix: float) -> float:
