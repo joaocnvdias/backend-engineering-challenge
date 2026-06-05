5443f37bb94cfb65004967b75b5daf2dcd4987fb
Dias <joaocnvdias@gmail.com>
2026-06-05 11:55:01 +0100

    feat: implemented sliding window solution

--- unbabel_cli/__main__.py
+++ unbabel_cli/__main__.py
@@ -1,9 +1,10 @@
 from parser import parse_from_cli, load_translation_events
 import datetime
+from collections import deque
 
 def clean_data(events_list: list) -> list:
     wanted_keys = ["timestamp", "duration"]
-    filtered_events_list = [{key: event_dict[key] for key in wanted_keys} for event_dict in events_list]
+    filtered_events_list = deque({key: event_dict[key] for key in wanted_keys} for event_dict in events_list)
 
     for event_dict in filtered_events_list:
         event_dict["timestamp"] = timestamp_to_unix(event_dict["timestamp"])
@@ -25,14 +26,26 @@ def main():
     args = parse_from_cli()
     translation_events = load_translation_events(file_location=args.input_file)
     filtered_events = clean_data(events_list=translation_events)
-    
     first_min = floor_to_minutes(filtered_events[0]["timestamp"])
     last_min = floor_to_minutes(filtered_events[-1]["timestamp"]) + 60
+    window_size_unix = args.window_size*60
+
 
     current = first_min
+    sliding_window = deque()
+    while current <= last_min:
+        for event in list(filtered_events): #cant iterate over deque if we change it mid loop
+            if event["timestamp"]>=current-window_size_unix and event["timestamp"]<current:
+                sliding_window.append(event)
+                filtered_events.popleft()
+            else: 
+                break #since elements are ordered, if one is outside of the window the following should to 
 
-    while first_min <= last_min:
-
+        for event in list(sliding_window): 
+            if event["timestamp"] < current-window_size_unix or event["timestamp"] >= current:
+                sliding_window.popleft()
+            else:
+                break #if an event is inside the window, then the rest will also be 
         current += 60
   
 
